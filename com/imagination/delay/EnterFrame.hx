dd22175fd3be538828820a38e1a57fed67e8928d
P.J.Shand <pete.shand@imagination.com>
2016-02-10 13:01:43 +1100

    Remove double mapping per callback

--- com/imagination/delay/EnterFrame.hx
+++ com/imagination/delay/EnterFrame.hx
@@ -1,7 +1,13 @@
 package com.imagination.delay;
 
+import haxe.ds.ObjectMap;
 import haxe.Timer;
 import lime.app.Application;
+
+#if flash
+import flash.Lib;
+import flash.events.Event;
+#end
 /**
  * ...
  * @author P.J.Shand
@@ -9,8 +15,9 @@ import lime.app.Application;
 class EnterFrame
 {
 	private static var enterFrameObjects = new Array<EnterFrameObject>();
-	private static var _started:Bool = false;
-	private static var started(get, set):Bool;
+	//private static var enterFrameObjects = new ObjectMap<Dynamic, EnterFrameObject>();
+	private static var _running:Bool = false;
+	private static var running(get, set):Bool;
 	
 	public function new() 
 	{
@@ -19,21 +26,41 @@ class EnterFrame
 	
 	private static function OnTick():Void
 	{
-		if (!started) return;
-		if (enterFrameObjects.length == 0) started = false;
 		for (i in 0...enterFrameObjects.length) 
 		{
 			enterFrameObjects[i].tick();
 		}
-		Timer.delay(OnTick, Std.int(1000 / Application.current.frameRate));
+		
+		#if (!flash)
+			if (running) Timer.delay(OnTick, Std.int(1000 / Application.current.frameRate));
+		#end
 	}
 	
 	static public function add(callback:Int->Void):Void 
 	{
-		started = true;
-		var enterFrameObject:EnterFrameObject = new EnterFrameObject(callback);
+		running = true;
+		var enterFrameObject:EnterFrameObject = getEnterFrameObject(callback);
+		if (enterFrameObject == null) {
+			enterFrameObject = new EnterFrameObject(callback);
+			enterFrameObjects.push(enterFrameObject);
+		}
 		enterFrameObject.start();
-		enterFrameObjects.push(enterFrameObject);
+	}
+	
+	static public function addAt(callback:Int->Void, index:Int):Void 
+	{
+		if (enterFrameObjects.length > index) {
+			var newEnterFrameObjects = new Array<EnterFrameObject>();	
+			for (i in 0...enterFrameObjects.length) 
+			{
+				if (i == index) enterFrameObjects.push(new EnterFrameObject(callback));
+				newEnterFrameObjects.push(enterFrameObjects[i]);
+			}
+			enterFrameObjects = newEnterFrameObjects;
+		}
+		else {
+			add(callback);
+		}
 	}
 	
 	static public function remove(callback:Int->Void):Void 
@@ -41,24 +68,54 @@ class EnterFrame
 		for (i in 0...enterFrameObjects.length) 
 		{
 			if (enterFrameObjects[i].callback == callback) {
-				enterFrameObjects[i].stop();
-				enterFrameObjects[i] = null;
+				var enterFrameObject:EnterFrameObject = enterFrameObjects[i];
+				enterFrameObject.stop();
+				enterFrameObject = null;
 				enterFrameObjects.splice(i, 1);
 			}
 		}
+		if (enterFrameObjects.length == 0) running = false;
 	}
 	
-	
-	private static function get_started():Bool
+	static private function getEnterFrameObject(callback:Int->Void):EnterFrameObject
 	{
-		return _started;
+		for (i in 0...enterFrameObjects.length) 
+		{
+			if (enterFrameObjects[i].callback == callback) {
+				return enterFrameObjects[i];
+			}
+		}
+		return null;
 	}
 	
-	private static function set_started(value:Bool):Bool
+	private static function get_running():Bool
 	{
-		if (_started == value) return value;
-		_started = value;
-		if (_started) OnTick();
-		return _started;
+		return _running;
 	}
+	
+	private static function set_running(value:Bool):Bool
+	{
+		if (_running == value) return value;
+		_running = value;
+		if (_running) {
+			#if flash
+				Lib.current.stage.addEventListener(Event.ENTER_FRAME, Update);
+			#else
+				OnTick();
+			#end
+		}
+		else {
+			#if flash
+				Lib.current.stage.removeEventListener(Event.ENTER_FRAME, Update);
+			#end
+		}
+		return _running;
+	}
+	
+	#if flash
+	static private function Update(e:Event):Void 
+	{
+		OnTick();
+	}
+	#end
 }
